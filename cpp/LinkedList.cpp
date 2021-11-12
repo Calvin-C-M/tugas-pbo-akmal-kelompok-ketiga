8346fb0244279891e6b8f6ba2f091879cded78b3
Calvin-C-M <montolalu28@gmail.com>
2021-11-12 23:35:56 +0700

    Added search algorithm cpp

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -72,4 +72,97 @@ class LinkedList {
                 curr=curr->getNext();
             }
         }
+
+        void searchString(int cari, string data) {
+            /*
+
+            cari = 1; Mencari Nama
+            cari = 2; Mencari NPM
+
+            */
+
+            bool found=false;
+            Node *curr=this->head;
+            string dicari="";
+            while(curr != nullptr) {
+                switch(cari) {
+                    case 1 : dicari=curr->getMahasiswa()->getNama();
+                    case 2 : dicari=curr->getMahasiswa()->getNpm();
+                }
+
+                if(dicari==data) {
+                    found=true;
+                    curr->getMahasiswa()->print();
+                }
+
+                curr=curr->getNext();
+            }
+
+            if(!found) {
+                cout << "Data yang anda cari tidak dapat ditemukan";
+            }
+        }
+
+        void searchNilai(int cari, float nilai) {
+            /*
+            
+            cari = 1; Mencari nilai penguji 1
+            cari = 2; Mencari nilai penguji 2
+            cari = 3; Mencari nilai pembimbing
+            cari = 4; Mencari nilai sidang
+
+            */
+
+            bool found=false;
+            Node *curr=this->head;
+            float dicari=0;
+
+            while(curr != nullptr) {
+                switch(cari) {
+                   case 1 : dicari=curr->getMahasiswa()->getPenguji1().getNilai();
+                   case 2 : dicari=curr->getMahasiswa()->getPenguji2().getNilai();
+                   case 3 : dicari=curr->getMahasiswa()->getPembimbing().getNilai();
+                   case 4 : dicari=curr->getMahasiswa()->getNilaiSidang().getNilai();
+                }
+
+                if(dicari==nilai) {
+                   found=true;
+                   curr->getMahasiswa()->print();
+                }
+
+                curr=curr->getNext();
+            }
+
+            if(!found) {
+               cout << "Data yang anda cari tidak dapat ditemukan";
+            }
+        }
+
+        void searchChar(int cari, char karakter) {
+            /*
+
+            cari = 1; Mencari huruf mutu
+
+            */
+            bool found=false;
+            Node *curr=this->head;
+            char dicari='A';
+
+            while(curr != nullptr) {
+                switch(cari) {
+                    case 1 : dicari=curr->getMahasiswa()->getHurufMutu(curr->getMahasiswa()->getNilaiSidang().getNilai());
+                }
+
+                if(dicari==karakter) {
+                    found=true;
+                    curr->getMahasiswa()->print();
+                }
+
+                curr=curr->getNext();
+            }
+
+            if(!found) {
+                cout << "Data yang anda cari tidak dapat ditemukan";
+            }
+        }
 };
