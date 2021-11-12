9ba54db0d1c61e08ac942585e4ef771e6b79d986
Calvin-C-M <montolalu28@gmail.com>
2021-11-12 23:10:34 +0700

    Added search algorithm java

--- java/LinkedList.java
+++ java/LinkedList.java
@@ -67,4 +67,75 @@ public class LinkedList {
             curr=curr.getNext();
         }
     }
+    
+    public void searchString(String cari, String data) {
+        boolean found=false;
+        Node curr=this.head;
+        String dicari=new String();
+
+        while(curr != null) {
+            switch(cari) {
+                case "Nama" : dicari=curr.getMahasiswa().getNama();
+                case "NPM" : dicari=curr.getMahasiswa().getNpm();
+            }
+
+            if(dicari==data) {
+                found=true;
+                curr.getMahasiswa().print();
+            }
+
+            curr=curr.getNext();
+        }
+
+        if(!found) {
+            System.out.println("Data tidak ada yang ditemukan");
+        }
+    }
+
+    public void searchNilai(String cari, float nilai) {
+        boolean found=false;
+        Node curr=this.head;
+        float dicari=0;
+        
+        while(curr != null) {
+            switch(cari) {
+                case "Penguji 1" : dicari=curr.getMahasiswa().getPenguji1().getNilai();
+                case "Penguji 2" : dicari=curr.getMahasiswa().getPenguji2().getNilai();
+                case "Pembimbing" : dicari=curr.getMahasiswa().getPembimbing().getNilai();
+                case "Nilai Sidang" : dicari=curr.getMahasiswa().getNilaiSidang().getNilai();
+            }
+            
+            if(dicari==nilai) {
+                found=true;
+                curr.getMahasiswa().print();
+            }
+
+            curr=curr.getNext();
+        }
+
+        if(!found) {
+            System.out.println("Data tidak ada yang ditemukan");
+        }
+    }
+
+    public void searchChar(String cari, char karakter) {
+        boolean found=false;
+        Node curr=this.head;
+        char dicari='A';
+
+        while(curr != null) {
+            switch(cari) {
+                case "Huruf Mutu" : dicari=curr.getMahasiswa().getHurufMutu(curr.getMahasiswa().getNilaiSidang().getNilai());
+            }
+
+            if(dicari==karakter) {
+                found=true;
+                curr.getMahasiswa().print();
+            }
+        }
+
+        if(!found) {
+            System.out.println("Data tidak ada yang ditemukan");
+        }
+    }
 }
