9921d58aadd9281fa1c8ac8b7838bc87b58a2b03
Calvin-C-M <montolalu28@gmail.com>
2021-11-17 13:13:23 +0700

    Jadiin loop untuk menunya

--- java/Main.java
+++ java/Main.java
@@ -23,103 +23,85 @@ public class Main {
         System.out.println(" 8. Tampil Data");
         System.out.println(" 9. Keluar");
         System.out.println("===========================");
-        System.out.print(" Pilih : ");
-    }
-
-    public static void backToMenu(){
-        System.out.println("===========================");
-        System.out.println(" Kembali ke menu?");
-        System.out.println(" 1. Yes");
-        System.out.println(" 2. No");
-        System.out.println("===========================");
-        System.out.print(" Pilih : ");
-        int pilih = input.nextInt();
-        if(pilih == 1){
-            //continue ;
-        } else {
-            System.exit(0);
-        }
     }
 
     public static void main(String[] args) {
         LinkedList myNilai = new LinkedList();
         Element newElement = new Element();
-        int inMenu, inBack;
-        menu();
-        inMenu = input.nextInt();
+        boolean programRunning=true;
 
-        switch (inMenu) {
-            case 1:
-                System.out.println("===========================");
-                newElement = myNilai.createElmt();
-                myNilai.insertFirst(newElement);
-                backToMenu();
-                break;
+        while(programRunning) {
+            menu();
+            System.out.print(" Pilih : ");
+            int inMenu = input.nextInt();
 
-            case 2:
-                System.out.println("===========================");
-                newElement = myNilai.createElmt();
-                myNilai.insertLast(newElement);
-                backToMenu();
-                break;
+            switch (inMenu) {
+                case 1:
+                    System.out.println("===========================");
+                    newElement = myNilai.createElmt();
+                    myNilai.insertFirst(newElement);
+                    break;
 
-            case 3:
-                System.out.println("===========================");
-                myNilai.deleteFirst();
-                backToMenu();
-                break;
+                case 2:
+                    System.out.println("===========================");
+                    newElement = myNilai.createElmt();
+                    myNilai.insertLast(newElement);
+                    break;
 
-            case 4:
-                System.out.println("===========================");
-                myNilai.deleteLast();
-                backToMenu();
-                break;
+                case 3:
+                    System.out.println("===========================");
+                    myNilai.deleteFirst();
+                    break;
 
-            case 5:
-                System.out.println("===========================");
-                System.out.print("Cari mahasiswa dengan Nama/NPM : ");
-                String mhs = input.nextLine();
-                System.out.print("Masukkan " + mhs + " : ");
-                String data = input.nextLine();
-                header();
-                myNilai.searchString(mhs, data);
-                backToMenu();
-                break;
+                case 4:
+                    System.out.println("===========================");
+                    myNilai.deleteLast();
+                    break;
 
-            case 6:
-                System.out.println("===========================");
-                System.out.print("Cari nilai apa : ");
-                String jns = input.nextLine();
-                System.out.print("Masukkan nilai " + jns + " : ");
-                float nilai = input.nextFloat();
-                header();
-                myNilai.searchNilai(jns, nilai);
-                backToMenu();
-                break;
+                case 5:
+                    System.out.println("===========================");
+                    System.out.print("Cari mahasiswa dengan Nama/NPM : ");
+                    String mhs = input.nextLine();
+                    System.out.print("Masukkan " + mhs + " : ");
+                    String data = input.nextLine();
+                    header();
+                    myNilai.searchString(mhs, data);
+                    break;
 
-            case 7:
-                System.out.println("===========================");
-                System.out.print("Masukkan huruf : ");
-                char huruf = input.next().charAt(0);
-                header();
-                myNilai.searchNilai("Huruf Mutu", huruf);
-                backToMenu();
-                break;
+                case 6:
+                    System.out.println("===========================");
+                    System.out.print("Cari nilai apa : ");
+                    String jns = input.nextLine();
+                    System.out.print("Masukkan nilai " + jns + " : ");
+                    float nilai = input.nextFloat();
+                    header();
+                    myNilai.searchNilai(jns, nilai);
+                    break;
 
-            case 8:
-                System.out.println();
-                header();
-                myNilai.printList();
-                System.out.println("=============================================================================================================================");
-                backToMenu();
-                break;
+                case 7:
+                    System.out.println("===========================");
+                    System.out.print("Masukkan huruf : ");
+                    char huruf = input.next().charAt(0);
+                    header();
+                    myNilai.searchNilai("Huruf Mutu", huruf);
+                    break;
 
-            case 9:
-                System.exit(0);
-                break;
+                case 8:
+                    System.out.println();
+                    header();
+                    myNilai.printList();
+                    System.out.println("=============================================================================================================================");
+                    break;
 
-            default:
-                break;
+                case 9:
+                    programRunning=false;
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Ada kesalahan dalam memilih, silahkan coba lagi");
+                    break;
+            }
         }
     }
 }
